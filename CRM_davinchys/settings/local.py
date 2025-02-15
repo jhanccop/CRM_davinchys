5de96c1673bc7695a43f5fbc2f0f1a067234a333
jhanccop <jhanccop@gmail.com>
2025-02-15 08:44:52 -0500

    Production

--- CRM_davinchys/settings/local.py
+++ CRM_davinchys/settings/local.py
@@ -1,9 +1,9 @@
 import os
 from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["167.71.98.235"]
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
