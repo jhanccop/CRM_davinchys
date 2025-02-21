55ada89d26f0d4cb6fbe83dd24b4c151eec7757d
jhanccop <jhanccop@gmail.com>
2025-02-20 19:13:16 -0500

    add dominio

--- CRM_davinchys/settings/local.py
+++ CRM_davinchys/settings/local.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["167.71.98.235"]
+ALLOWED_HOSTS = ["167.71.98.235","erpdavinchy.cloud"]
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
